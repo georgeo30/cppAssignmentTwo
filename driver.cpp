c3065e2d7696e1a4b359bef5a1c361ca47fca963
georgeo <georgeosabu@gmail.com>
2020-02-27 14:49:46 +0200

    works for one input in cmd

--- driver.cpp
+++ driver.cpp
@@ -3,18 +3,25 @@
 #include "volimage.h"
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
     THNGEO002::VolImage v;
-    string baseName;
-    cout<<"Please input the base name"<<endl;
-    getline(cin,baseName);
     
-    v.readImages(baseName);
-    //v.diffmap(1,2,"outputDiffmap");
-    //v.extract(1,"outputExtract");
-    int bytes=v.volImageSize();
+    
+ 
+    string baseName;
+
+    if(argc==2){
+       
+    
+    v.readImages(argv[1]);
+        int bytes=v.volImageSize();
     int sizeOfSlices=v.getSlices();
     cout<<"Number of images: "<<sizeOfSlices<<endl;
     cout<<"Number of bytes required: "<<bytes<<endl;
+    }
+    
+    //v.diffmap(1,2,"outputDiffmap");
+    //v.extract(1,"outputExtract");
+    
 return 0;
 }
