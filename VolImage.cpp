daed4a167313f922742ecf3f27d31fc87bacbbcc
georgeo <georgeosabu@gmail.com>
2020-02-27 11:27:31 +0200

    Difference map working

--- VolImage.cpp
+++ VolImage.cpp
@@ -12,6 +12,7 @@ VolImage::VolImage()
 {
     width = 0;
     height = 0;
+    slicesNo = 0;
 } // default constructor - define in .cpp
 VolImage::~VolImage() {} // destructor - define in .cpp file
 // populate the object with images in stack and
@@ -23,7 +24,7 @@ bool VolImage::readImages(std::string baseName)
     //opening the .data file and going through it
     myfile.open(baseNameUrl, ios::out | ios::app | ios::binary);
     string line;
-    int slicesNo;
+
     //if .data file is open go through and get the width, height and slicesNo
     if (myfile.is_open())
     {
@@ -38,40 +39,55 @@ bool VolImage::readImages(std::string baseName)
 
         myfile.close();
 
-        cout << slicesNo << " " << width << " " << height<<endl;
+        cout << slicesNo << " " << width << " " << height << endl;
     }
 
     for (int i = 0; i < slicesNo; i++)
     {
-        string sliceUrl = "./" + baseName + "/" + baseName +to_string(i)+".raw";
+        string sliceUrl = "./" + baseName + "/" + baseName + to_string(i) + ".raw";
         ifstream rawFile;
         rawFile.open(sliceUrl, ios::binary);
 
         slices.push_back(new unsigned char *[height + 1]);
-         string sliceRow;
-         
+        string sliceRow;
+
         for (int j = 0; j < height; j++)
         {
-           
+
             slices[i][j] = new unsigned char[width + 1];
             for (int k = 0; k < width; k++)
             {
-                slices[i][j][k]=rawFile.get();
+                slices[i][j][k] = rawFile.get();
             }
         }
-       rawFile.close();
+        rawFile.close();
     }
-//     ofstream outfile;
-//    outfile.open("afile.dat");
-//     for(int i=0;i<height;i++){
-//         for(int j=0;j<width;j++){
-//             outfile<<slices[0][i][j];
-//         }
-//     }
+    //     ofstream outfile;
+    //    outfile.open("afile.dat");
+    //     for(int i=0;i<height;i++){
+    //         for(int j=0;j<width;j++){
+    //             outfile<<slices[0][i][j];
+    //         }
+    //     }
     return 0;
 }
-// compute difference map and write out; define in .cpp
-void VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix) {}
+// compute difference map and write out;
+void VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix)
+{
+
+    ofstream outfile;
+    outfile.open("output_prefix.raw");
+    
+            for (int j = 0; j < height; j++)
+            {
+                for (int k = 0; k < width; k++)
+                {
+                    outfile << (slices[sliceI][j][k]-slices[sliceJ][j][k]);
+                }
+            }
+        
+    
+}
 // extract slice sliceId and write to output - define in .cpp
 void VolImage::extract(int sliceId, std::string output_prefix) {}
 // number of bytes uses to store image data bytes
